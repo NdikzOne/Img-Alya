a1a354bd0b5bef45968a2045b9a93d0259e36a40
Nd <baksocloud@gmail.com>
2025-05-18 19:46:48 +0700

    Delete IP 202.157.185.130 via bot

--- database/Accesip.js
+++ database/Accesip.js
@@ -36,10 +36,6 @@
         "status": "tidakdiverif"
       },
       {
-        "ip": "202.157.185.130",
-        "status": "terverifikasi"
-      },
-      {
         "ip": "128.199.81.216",
         "status": "terverifikasi"
       },
