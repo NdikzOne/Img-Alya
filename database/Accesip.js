e10bc5c1daa91665214fed187a04e64be4a1c385
Nd <baksocloud@gmail.com>
2025-09-03 06:47:01 +0700

    Delete IP 194.62.248.52 via API

--- database/Accesip.js
+++ database/Accesip.js
@@ -64,10 +64,6 @@
         "status": "terverifikasi"
       },
       {
-        "ip": "194.62.248.52",
-        "status": "terverifikasi"
-      },
-      {
         "ip": "165.22.98.49",
         "status": "terverifikasi"
       }
